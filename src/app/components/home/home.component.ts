71d7e1e90f13adf38744340aa905b7fddbc876d9
shykiu77 <carlosgabrielsns@gmail.com>
2024-08-07 20:52:07 -0300

    altera quantidade padrão de daysAgo para 14

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -58,7 +58,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   totalRecords: number = 0;
   priceMin: number | null = null;
   priceMax: number | null = null;
-  daysAgo: number = 7;
+  daysAgo: number = 14;
 
   @ViewChild('scroller') scroller!: any;
 
@@ -142,7 +142,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.sortOrder = 'desc';
     this.priceMin = null;
     this.priceMax = null;
-    this.daysAgo = 7;
+    this.daysAgo = 14;
     this.loadData({ first: 0, last: 0 }, true);
   }
 
